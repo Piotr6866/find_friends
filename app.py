d801423436eea1896ee117a0e52ba7d7866afb32
PiotrK <Piotr_Kotecki@hotmail.com>
2026-05-17 18:23:39 +0200

    Add all project files

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ import pandas as pd  # type: ignore
 from pycaret.clustering import load_model, predict_model  # type: ignore
 import plotly.express as px  # type: ignore
 
-MODEL_NAME = 'welcome_survey_clustering_pipeline_v1'
+MODEL_NAME = 'welcome_survey_clustering_pipeline_v2'
 
-DATA = 'welcome_survey_simple_v1.csv'
+DATA = './welcome_survey_simple_v2.csv'
 
 CLUSTER_NAMES_AND_DESCRIPTIONS = 'welcome_survey_cluster_names_and_descriptions_v1.json'
 
